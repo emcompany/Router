1a5352a192952f73a2a0d9224cd2562b43b6d540
emcompany <ericmathewem8@gmail.com>
2023-06-17 14:04:06 +0530

    scndupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import Contact from "./Pages/Contact"
+import About from "./Pages/About"
+import NotFound from './Pages/NotFound'
+import Header from './units/Header'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header></Header>
+      <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/contact" element={<Contact/>} />
+    <Route path="*" element={<NotFound/>} />
+  </Routes></div>
   )
 }
+
---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './NotFound.css'; 
+export default function NotFound() {
+
+
+
+  
+  return (
+    
+
+<div class="error-pg">
+
+<div class="error-number">
+  <div class="number left-coffee">4</div>
+  <div class="coffee-mug"></div>
+  <div class="number right-coffee">4</div>
+</div>
+<div class="sm-screen">404</div>
+<div class="mean-msg">
+ Nothing to see here, <a href="#">go back home!</a>
+</div>
+
+</div>
+
+
+  )
+}
